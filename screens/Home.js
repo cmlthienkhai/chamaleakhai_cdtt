dfb19590112f2c124ac5369f58d4bb86998bf209
khai <tanthienthieu@gmail.com>
2023-12-26 19:23:14 +0700

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,378 @@
+import {StyleSheet,Text,View,SafeAreaView,Platform,ScrollView,Pressable,TextInput,Image,} from "react-native";
+import React, { useState, useEffect, useCallback, useContext } from "react";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { SliderBox } from "react-native-image-slider-box";
+import axios from "axios";
+import ProductItem from "../components/ProductItem";
+import DropDownPicker from "react-native-dropdown-picker";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { BottomModal, SlideAnimation, ModalContent } from "react-native-modals";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UserType } from "./UserContext";
+import jwt_decode from "jwt-decode";
+
+const HomeScreen = () => {
+  const list = [
+    {
+      
+      name: "Trang chủ",
+    },
+    {
+     
+      name: "Sale",
+    },
+    {
+     
+      name: "SP 1",
+    },
+    {
+    
+      name: "SP 2",
+    },
+    {
+      
+      name: "SP 3",
+    },
+    {
+    
+      name: "SP 4",
+    },
+  ];
+  const images = [
+    "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/07/anh-bia-dep.jpg",
+    "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/07/anh-bia-dep-2.jpg",
+    "https://marketplace.canva.com/EAEwg8QYxDA/1/0/1600w/canva-%C4%91en-v%C3%A0-tr%E1%BA%AFng-b%E1%BA%A7u-tr%E1%BB%9Di-%C4%91%E1%BA%A7y-sao-%E1%BA%A3nh-b%C3%ACa-facebook-uoNHptrRpc8.jpg",
+  ];
+  const deals = [
+    
+  ];
+  const offers = [
+    {
+      id: "0",
+      title:
+        "Oppo Enco Air3 Pro True Wireless in Ear Earbuds with Industry First Composite Bamboo Fiber, 49dB ANC, 30H Playtime, 47ms Ultra Low Latency,Fast Charge,BT 5.3 (Green)",
+      offer: "72% off",
+      oldPrice: 7500,
+      price: 4500,
+      image:
+        "https://m.media-amazon.com/images/I/61a2y1FCAJL._AC_UL640_FMwebp_QL65_.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/61a2y1FCAJL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71DOcYgHWFL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71LhLZGHrlL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61Rgefy4ndL._SX679_.jpg",
+      ],
+      color: "Green",
+      size: "Normal",
+    },
+    {
+      id: "1",
+      title:
+        "Fastrack Limitless FS1 Pro Smart Watch|1.96 Super AMOLED Arched Display with 410x502 Pixel Resolution|SingleSync BT Calling|NitroFast Charging|110+ Sports Modes|200+ Watchfaces|Upto 7 Days Battery",
+      offer: "40%",
+      oldPrice: 7955,
+      price: 3495,
+      image: "https://m.media-amazon.com/images/I/41mQKmbkVWL._AC_SY400_.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/71h2K2OQSIL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71BlkyWYupL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71c1tSIZxhL._SX679_.jpg",
+      ],
+      color: "black",
+      size: "Normal",
+    },
+    {
+      id: "2",
+      title: "Aishwariya System On Ear Wireless On Ear Bluetooth Headphones",
+      offer: "40%",
+      oldPrice: 7955,
+      price: 3495,
+      image: "https://m.media-amazon.com/images/I/41t7Wa+kxPL._AC_SY400_.jpg",
+      carouselImages: ["https://m.media-amazon.com/images/I/41t7Wa+kxPL.jpg"],
+      color: "black",
+      size: "Normal",
+    },
+    {
+      id: "3",
+      title:
+        "Fastrack Limitless FS1 Pro Smart Watch|1.96 Super AMOLED Arched Display with 410x502 Pixel Resolution|SingleSync BT Calling|NitroFast Charging|110+ Sports Modes|200+ Watchfaces|Upto 7 Days Battery",
+      offer: "40%",
+      oldPrice: 24999,
+      price: 19999,
+      image: "https://m.media-amazon.com/images/I/71k3gOik46L._AC_SY400_.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/41bLD50sZSL._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://m.media-amazon.com/images/I/616pTr2KJEL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71wSGO0CwQL._SX679_.jpg",
+      ],
+      color: "Norway Blue",
+      size: "8GB RAM, 128GB Storage",
+    },
+  ];
+  const [products, setProducts] = useState([]);
+  const navigation = useNavigation();
+  const [open, setOpen] = useState(false);
+  const [addresses, setAddresses] = useState([]);
+  const [category, setCategory] = useState("jewelery");
+  const { userId, setUserId } = useContext(UserType);
+  const [selectedAddress,setSelectedAdress] = useState("");
+  console.log(selectedAddress)
+  const [items, setItems] = useState([
+    { label: "Men's clothing", value: "men's clothing" },
+    { label: "jewelery", value: "jewelery" },
+    { label: "electronics", value: "electronics" },
+    { label: "women's clothing", value: "women's clothing" },
+  ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.log("error message", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const onGenderOpen = useCallback(() => {
+    setCompanyOpen(false);
+  }, []);
+
+  const cart = useSelector((state) => state.cart.cart);
+  const [modalVisible, setModalVisible] = useState(false);
+  useEffect(() => {
+    if (userId) {
+      fetchAddresses();
+    }
+  }, [userId, modalVisible]);
+  const fetchAddresses = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/addresses/${userId}`
+      );
+      const { addresses } = response.data;
+
+      setAddresses(addresses);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = await AsyncStorage.getItem("authToken");
+      const decodedToken = jwt_decode(token);
+      const userId = decodedToken.userId;
+      setUserId(userId);
+    };
+
+    fetchUser();
+  }, []);
+  console.log("address", addresses);
+  return (
+    
+    <>
+      
+      <SafeAreaView
+        style={{
+          paddingTop: Platform.OS === "android" ? 20 : 0,  // Điều chỉnh giá trị paddingTop
+          flex: 1,
+          backgroundColor: "white",
+        }}
+      >
+        <ScrollView>
+          <View
+            style={{
+              backgroundColor: "#00CED1",
+              padding: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 30,  // Điều chỉnh giá trị marginTop
+            }}
+          >
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginHorizontal: 7,
+                gap: 10,
+                backgroundColor: "white",
+                borderRadius: 3,
+                height: 38,
+                flex: 1,
+              }}
+            >
+              <AntDesign
+                style={{ paddingLeft: 10 }}
+                name="search1"
+                size={22}
+                color="black"
+              />
+              <TextInput placeholder="Search Amazon.in" />
+            </Pressable>
+
+            <Feather name="mic" size={24} color="black" />
+          </View>
+
+         
+
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {list.map((item, index) => (
+              <Pressable
+                key={index}
+                style={{
+                  margin: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  style={{ width: 50, height: 50, resizeMode: "contain" }}
+                  source={{ uri: item.image }}
+                />
+
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 12,
+                    fontWeight: "500",
+                    marginTop: 5,
+                  }}
+                >
+                  {item?.name}
+                </Text>
+              </Pressable>
+            ))}
+          </ScrollView>
+
+          <SliderBox
+            images={images}
+            autoPlay
+            circleLoop
+            dotColor={"#13274F"}
+            inactiveDotColor="#90A4AE"
+            ImageComponentStyle={{ width: "100%" }}
+          />
+
+          <Text style={{ padding: 10, fontSize: 18, fontWeight: "bold" }}>
+            Flash Sale
+          </Text>
+
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {offers.map((item, index) => (
+              <Pressable
+                onPress={() =>
+                  navigation.navigate("Info", {
+                    id: item.id,
+                    title: item.title,
+                    price: item?.price,
+                    carouselImages: item.carouselImages,
+                    color: item?.color,
+                    size: item?.size,
+                    oldPrice: item?.oldPrice,
+                    item: item,
+                  })
+                }
+                style={{
+                  marginVertical: 10,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  style={{ width: 150, height: 150, resizeMode: "contain" }}
+                  source={{ uri: item?.image }}
+                />
+
+                <View
+                  style={{
+                    backgroundColor: "#E31837",
+                    paddingVertical: 5,
+                    width: 130,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop: 10,
+                    borderRadius: 4,
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 13,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Upto {item?.offer}
+                  </Text>
+                </View>
+              </Pressable>
+            ))}
+          </ScrollView>
+
+          <Text
+            style={{
+              height: 1,
+              borderColor: "#D0D0D0",
+              borderWidth: 2,
+              marginTop: 15,
+            }}
+          />
+
+          <View
+            style={{
+              marginHorizontal: 10,
+              marginTop: 20,
+              width: "45%",
+              marginBottom: open ? 50 : 15,
+            }}
+          >
+            <DropDownPicker
+              style={{
+                borderColor: "#B7B7B7",
+                height: 30,
+                marginBottom: open ? 120 : 15,
+              }}
+              open={open}
+              value={category} //genderValue
+              items={items}
+              setOpen={setOpen}
+              setValue={setCategory}
+              setItems={setItems}
+              placeholder="choose category"
+              placeholderStyle={styles.placeholderStyles}
+              onOpen={onGenderOpen}
+              // onChangeValue={onChange}
+              zIndex={3000}
+              zIndexInverse={1000}
+            />
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            {products
+              ?.filter((item) => item.category === category)
+              .map((item, index) => (
+                <ProductItem item={item} key={index} />
+              ))}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+
+    </>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
